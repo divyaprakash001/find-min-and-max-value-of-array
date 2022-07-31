ff480788e56827ddaf1b3c205a08ad26175ffc6a
Divya Prakash <95012051+divyaprakash-programmer@users.noreply.github.com>
2022-07-31 23:38:08 +0530

    Add files via upload

---
+++ finding_minimum_maximum_value_in_array_using_methods_in_java.java
@@ -0,0 +1,36 @@
+package com.divya;    // your package here
+
+public class finding_minimum_maximum_value_in_array_using_methods_in_java {
+
+    static void findMin(int arr[])    // using methods find minimum value
+    {
+        int min = arr[0];
+        for(int element : arr)   // using for each loop // you can use for loop also
+        {
+            if(min>element)
+            {
+                min = element;
+            }
+        }
+        System.out.println( "The minimum value in this array is "+min);
+    }
+
+    static void findMax(int arr[])    // using methods find maximum value
+    {
+        int max = arr[0];
+        for(int element : arr)   // using for each loop // you can use for loop also
+        {
+            if(max<element)
+            {
+                max = element;
+            }
+        }
+        System.out.println( "The maximum value in this array is "+max);
+    }
+
+    public static void main(String[] args) {
+        int []arra = {22,34,5,7,53,44,443,3};
+        findMin(arra);
+        findMax(arra);
+    }
+}
